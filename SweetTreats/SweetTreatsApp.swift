e646cabc076421e14c708d403d376570532753f8
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-07-28 23:38:31 -0500

    Initial Commit

---
+++ SweetTreats/SweetTreatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetTreatsApp.swift
+//  SweetTreats
+//
+//  Created by user263110 on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SweetTreatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
