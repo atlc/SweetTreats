61d3213975cfa97e771f0b7c16d6dc79635ba23f
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-07-29 04:03:00 -0500

    UI/UX improvements on the detailse details page with organized sections and a modal for full recipe steps

---
+++ Utilities/Modal.swift
@@ -0,0 +1,8 @@
+//
+//  Modal.swift
+//  SweetTreats
+//
+//  Created by user263110 on 7/29/24.
+//
+
+import Foundation
---
+++ Views/FullInstructions.swift
@@ -0,0 +1,8 @@
+//
+//  FullInstructions.swift
+//  SweetTreats
+//
+//  Created by user263110 on 7/29/24.
+//
+
+import Foundation
